2ae59901676a3b172550a357cc3a63a6aef9f0c7
Bala Juluri <plasmon360@gmail.com>
2020-02-04 17:36:10 -0800

    added vis2 with more advanced features

---
+++ VIS2/main.py
@@ -0,0 +1,226 @@
+import pandas as pd
+from bokeh.io import curdoc
+from collections import defaultdict
+from bokeh.plotting import figure, show, save
+from bokeh.models import Plot, Label, CDSView, Filter, Slider, CustomJS, CustomJSFilter, GroupFilter, BooleanFilter
+from bokeh.models.glyphs import Text
+from bokeh.layouts import column, Row, layout
+from bokeh.models import ColumnDataSource
+from bokeh.models.widgets import Div
+from bokeh.transform import factor_cmap
+
+
+def pingala(m, size=2):
+
+    if m < size:
+        return [[0] * m]
+    elif m == size:
+        return [[0] * size, [1] * size]
+    else:
+        result = []
+        seeds = [[0] * size]
+        seeds += [[0] * i + [1] * size for i in range(size)]
+        for seed in seeds:
+            for item in pingala(m - len(seed), size):
+                if not len(seed + item) > m:
+                    result.append(seed + item)
+        return result
+
+
+def find_first_non_zero(mylist):
+    i = 0
+    while i < len(mylist):
+        if mylist[i] != 0:
+            return i
+        else:
+            i = i + 1
+    return 0
+
+
+size = 2
+
+data = defaultdict(list)
+slider_initial_val = 5
+slider_final_val = 10
+
+for word_length in range(slider_initial_val - 2, slider_final_val + 1):
+    results = pingala(word_length, size)
+
+    results.sort(key=lambda x: (
+        x[-1] == 1,
+        sum(x),
+        find_first_non_zero(x),
+    ))
+
+    for height_id, result in enumerate(results):
+        i = 0
+        while i < len(result):
+            if result[i] == 1:
+                data['x'].append(i + size / 4)
+                data['type'].append('long')
+                data['width'].append(2 - 0.05)
+                i = i + size
+            else:
+                data['x'].append(i)
+                data['type'].append('short')
+                data['width'].append(1 - 0.05)
+                i = i + 1
+            data['y'].append(height_id)
+            data['word_length'].append(word_length)
+            data['ends_with_long'].append(result[-1] == 1)
+
+# data = pd.DataFrame.from_dict(data)
+source = ColumnDataSource(data)
+
+all_combinations = Div(text="""<span class="display-1">""" +
+                       str(slider_initial_val) + """</span>""",
+                       name='all_combinations')
+all_combinations.css_classes = ['combinations_style1']
+word_length_div = Div(text="""<span class="display-2">""" +
+                      str(slider_initial_val) + """</span>""",
+                      name='word_length')
+word_length_div.css_classes = ['wordlength_style1']
+
+# Create the slider that modifies the filtered indices
+# Create the slider that modifies the filtered indices
+# I am just creating one that shows 0 to 100% of the existing data rows
+slider = Slider(start=slider_initial_val,
+                end=slider_final_val,
+                value=slider_initial_val,
+                step=1,
+                title="Word Length",
+                name='word_length_slider')
+
+# This callback is crucial, otherwise the filter will not be triggered when the slider changes
+callback = CustomJS(args=dict(source=source,
+                              all_combinations=all_combinations,
+                              word_length_div=word_length_div,
+                              slider=slider),
+                    code="""
+   source.change.emit();
+""")
+slider.js_on_change('value', callback)
+
+# Define the custom filter to return the indices from 0 to the desired percentage of total data rows. You could also compare against values in source.data
+js_filter_n = CustomJSFilter(args=dict(
+    slider=slider,
+    source=source,
+),
+                             code='''
+            var indices = [];
+            console.log(source.data['word_length'])
+            for (var i = 0; i <= source.data['word_length'].length; i++){
+                if (source.data['word_length'][i] == slider.value) {
+                    indices.push(i)
+                }
+            }
+            return indices
+''')
+
+js_filter_n_1 = CustomJSFilter(args=dict(
+    slider=slider,
+    source=source,
+),
+                               code='''
+            var indices = [];
+            for (var i = 0; i <= source.data['word_length'].length; i++){
+                if (source.data['word_length'][i] == slider.value-1) {
+                    indices.push(i)
+                }
+            }
+            return indices
+''')
+js_filter_n_2 = CustomJSFilter(args=dict(
+    slider=slider,
+    source=source,
+),
+                               code='''
+            var indices = [];
+            for (var i = 0; i <= source.data['word_length'].length; i++){
+                if (source.data['word_length'][i] == slider.value-2) {
+                    indices.push(i)
+                }
+            }
+            return indices
+''')
+
+not_ends_with_long_view = CDSView(
+    source=source,
+    filters=[
+        BooleanFilter([not item for item in source.data['ends_with_long']]),
+        js_filter_n
+    ])
+
+ends_with_long_view = CDSView(
+    source=source,
+    filters=[BooleanFilter(source.data['ends_with_long']), js_filter_n])
+
+
+def myfig(name, view, height, width=400):
+
+    fig = figure(
+        toolbar_location=None,
+        name=name,
+    )
+    fig.axis.visible = False
+    fig.xgrid.visible = False
+    fig.ygrid.visible = False
+    fig.outline_line_color = 'Blue'
+    fig.min_border = 0
+    fig.x_range.range_padding = 0
+    fig.y_range.range_padding = 0.03
+    fig.plot_height = height
+    fig.sizing_mode = "fixed"
+    groups = ['long', 'short']
+    shorts = fig.rect(x='x',
+                      y='y',
+                      source=source,
+                      view=view,
+                      width='width',
+                      height=0.7,
+                      alpha=0.8,
+                      color=factor_cmap(field_name='type',
+                                        palette=['red', 'grey'],
+                                        factors=groups))
+
+    return fig
+
+
+HEIGHT = 400
+fig_n_not_long_ending = myfig(
+    name="fig_n",
+    view=CDSView(source=source,
+                 filters=[
+                     BooleanFilter(
+                         [not item for item in source.data['ends_with_long']]),
+                     js_filter_n
+                 ]),
+    height=HEIGHT,
+)
+fig_n_long_ending = myfig(
+    name="fig_n",
+    view=CDSView(
+        source=source,
+        filters=[BooleanFilter(source.data['ends_with_long']), js_filter_n]),
+    height=int(HEIGHT / 1.618),
+)
+fig_n_long_ending.x_range = fig_n_not_long_ending.x_range
+
+fig_n = column([fig_n_long_ending, fig_n_not_long_ending], name='fig_n')
+fig_n_1 = myfig(name="fig_n_1",
+                view=CDSView(source=source, filters=[js_filter_n_1]),
+                height=HEIGHT)
+
+fig_n_2 = myfig(name="fig_n_2",
+                view=CDSView(source=source, filters=[js_filter_n_2]),
+                height=int(HEIGHT / 1.618))
+fig_n_1.x_range = fig_n_not_long_ending.x_range
+fig_n_2.x_range = fig_n_not_long_ending.x_range
+
+curdoc().add_root(fig_n)
+curdoc().add_root(fig_n_1)
+curdoc().add_root(fig_n_2)
+curdoc().add_root(slider)
+curdoc().add_root(word_length_div)
+curdoc().add_root(all_combinations)
+curdoc().title = "Pingala"
